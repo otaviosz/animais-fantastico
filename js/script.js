289d6f8841e01fd25850da869ed3d406c586b5e4
Otávio <otaviodiascc@gmail.com>
2024-07-10 20:44:25 -0300

    adicionado slide-fotos

--- js/script.js
+++ js/script.js
@@ -9,6 +9,7 @@ import ScrollAnima from './modules/scroll-anima.js';
 import DropdownMenu from './modules/dropdown-menu.js';
 import fetchAnimais from './modules/fetch-animais.js';
 import fetchBitcoin from './modules/fetch-bitcoin.js';
+import { SlideNav } from './modules/slide.js';
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
 scrollSuave.init();
@@ -42,3 +43,9 @@ funcionamento.init();
 
 fetchAnimais('./animaisapi.json', '.numeros-grid');
 fetchBitcoin('https://blockchain.info/ticker', '.btc-preco');
+
+
+const slide = new SlideNav('.slide', '.wrapper');
+slide.init();
+
+slide.addControl('.custom-controls');
